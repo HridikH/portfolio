51a9a7dd649e38c1930766add05a8591ebf8bcc0
Hridik Hingorani <hridik7@Hridiks-MacBook-Air.local>
2026-04-16 12:32:07 -0500

    WE'RE BACK

---
+++ projects/UR3/lab5_exec.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python
+
+import sys
+import copy
+import time
+import rospy
+
+import numpy as np
+from lab5_header import *
+from lab5_func import *
+from blob_search import *
+
+
+# ========================= Student's code starts here =========================
+
+# Position for UR3 not blocking the camera
+go_away = [270 * PI / 180.0, -90 * PI / 180.0, 90 * PI / 180.0,
+           -90 * PI / 180.0, -90 * PI / 180.0, 135 * PI / 180.0]
+
+# Store world coordinates of green and yellow blocks
+xw_yw_G = []
+xw_yw_Y = []
+
+# ========================= Student's code ends here ===========================
+
+################ Pre-defined parameters and functions no need to change below ################
+
+# 20Hz
+SPIN_RATE = 20
+
+# UR3 home location
+home = [0 * PI / 180.0, 0 * PI / 180.0, 0 * PI / 180.0,
+        0 * PI / 180.0, 0 * PI / 180.0, 0 * PI / 180.0]
+
+# UR3 current position, using home position for initialization
+current_position = copy.deepcopy(home)
+
+thetas = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+digital_in_0 = 0
+analog_in_0 = 0.0
+
+suction_on = True
+suction_off = False
+
+current_io_0 = False
+current_position_set = False
+
+image_shape_define = False
+
+
+def input_callback(msg):
+    global digital_in_0
+    digital_in_0 = msg.DIGIN
+    digital_in_0 = digital_in_0 & 1
+
+
+def position_callback(msg):
+    global thetas
+    global current_position
+    global current_position_set
+
+    thetas[0] = msg.position[0]
+    thetas[1] = msg.position[1]
+    thetas[2] = msg.position[2]
+    thetas[3] = msg.position[3]
+    thetas[4] = msg.position[4]
+    thetas[5] = msg.position[5]
+
+    current_position[0] = thetas[0]
+    current_position[1] = thetas[1]
+    current_position[2] = thetas[2]
+    current_position[3] = thetas[3]
+    current_position[4] = thetas[4]
+    current_position[5] = thetas[5]
+
+    current_position_set = True
+
+
+def gripper(pub_cmd, loop_rate, io_0):
+
+    global SPIN_RATE
+    global thetas
+    global current_io_0
+    global current_position
+
+    error = 0
+    spin_count = 0
+    at_goal = 0
+
+    current_io_0 = io_0
+
+    driver_msg = command()
+    driver_msg.destination = current_position
+    driver_msg.v = 1.0
+    driver_msg.a = 1.0
+    driver_msg.io_0 = io_0
+    pub_cmd.publish(driver_msg)
+
+    while (at_goal == 0):
+        if (abs(thetas[0] - driver_msg.destination[0]) < 0.0005 and \
+            abs(thetas[1] - driver_msg.destination[1]) < 0.0005 and \
+            abs(thetas[2] - driver_msg.destination[2]) < 0.0005 and \
+            abs(thetas[3] - driver_msg.destination[3]) < 0.0005 and \
+            abs(thetas[4] - driver_msg.destination[4]) < 0.0005 and \
+            abs(thetas[5] - driver_msg.destination[5]) < 0.0005):
+            at_goal = 1
+
+        loop_rate.sleep()
+
+        if (spin_count > SPIN_RATE * 5):
+            pub_cmd.publish(driver_msg)
+            rospy.loginfo("Just published again driver_msg")
+            spin_count = 0
+
+        spin_count = spin_count + 1
+
+    return error
+
+
+def move_arm(pub_cmd, loop_rate, dest, vel, accel):
+
+    global thetas
+    global SPIN_RATE
+
+    error = 0
+    spin_count = 0
+    at_goal = 0
+
+    driver_msg = command()
+    driver_msg.destination = dest
+    driver_msg.v = vel
+    driver_msg.a = accel
+    driver_msg.io_0 = current_io_0
+    pub_cmd.publish(driver_msg)
+
+    loop_rate.sleep()
+
+    while (at_goal == 0):
+        if (abs(thetas[0] - driver_msg.destination[0]) < 0.0005 and \
+            abs(thetas[1] - driver_msg.destination[1]) < 0.0005 and \
+            abs(thetas[2] - driver_msg.destination[2]) < 0.0005 and \
+            abs(thetas[3] - driver_msg.destination[3]) < 0.0005 and \
+            abs(thetas[4] - driver_msg.destination[4]) < 0.0005 and \
+            abs(thetas[5] - driver_msg.destination[5]) < 0.0005):
+            at_goal = 1
+
+        loop_rate.sleep()
+
+        if (spin_count > SPIN_RATE * 5):
+            pub_cmd.publish(driver_msg)
+            rospy.loginfo("Just published again driver_msg")
+            spin_count = 0
+
+        spin_count = spin_count + 1
+
+    return error
+
+################ Pre-defined parameters and functions no need to change above ################
+
+
+def move_block(pub_cmd, loop_rate, start_xw_yw_zw, target_xw_yw_zw, vel, accel):
+    """
+    start_xw_yw_zw: where to pick up a block in global coordinates
+    target_xw_yw_zw: where to place the block in global coordinates
+    """
+    # ========================= Student's code starts here =========================
+
+    x, y, z = start_xw_yw_zw[0][0], start_xw_yw_zw[1][0], 32
+    start_dest = lab_invk(x, y, z, 90)
+    move_arm(pub_cmd, loop_rate, start_dest, vel, accel)
+
+    error = 0
+
+    # ========================= Student's code ends here ===========================
+
+    return error
+
+
+class ImageConverter:
+
+    def __init__(self, SPIN_RATE):
+        self.bridge = CvBridge()
+        self.image_pub = rospy.Publisher(
+            "/image_converter/output_video", Image, queue_size=10)
+        self.image_sub = rospy.Subscriber(
+            "/cv_camera_node/image_raw", Image, self.image_callback)
+        self.loop_rate = rospy.Rate(SPIN_RATE)
+
+        while (rospy.is_shutdown()):
+            print("ROS is shutdown!")
+
+    def image_callback(self, data):
+        global xw_yw_O
+        global xw_yw_G
+
+        try:
+            raw_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+        cv_image = cv2.flip(raw_image, -1)
+        cv2.line(cv_image, (0, 50), (640, 50), (0, 0, 0), 5)
+
+        g = blob_search(cv_image, "green")
+        o = blob_search(cv_image, "orange")
+
+        if len(g) != 0:
+            xw_yw_G = g
+        if len(o) != 0:
+            xw_yw_O = o
+
+
+def main():
+
+    global go_away
+    global xw_yw_R
+    global xw_yw_G
+
+    rospy.init_node('lab5node')
+
+    pub_command = rospy.Publisher('ur3/command', command, queue_size=10)
+
+    sub_position = rospy.Subscriber(
+        'ur3/position', position, position_callback)
+    sub_input = rospy.Subscriber(
+        'ur3/gripper_input', gripper_input, input_callback)
+
+    while (rospy.is_shutdown()):
+        print("ROS is shutdown!")
+
+    loop_rate = rospy.Rate(SPIN_RATE)
+
+    vel = 4.0
+    accel = 4.0
+    move_arm(pub_command, loop_rate, go_away, vel, accel)
+
+    ic = ImageConverter(SPIN_RATE)
+    time.sleep(5)
+
+    # ========================= Student's code starts here =========================
+
+    if len(xw_yw_O) != 0:
+        move_block(pub_command, loop_rate, xw_yw_O[0], 0, vel, accel)
+
+    # ========================= Student's code ends here ===========================
+
+    move_arm(pub_command, loop_rate, go_away, vel, accel)
+    rospy.loginfo("Task Completed!")
+    print("Use Ctrl+C to exit program")
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
